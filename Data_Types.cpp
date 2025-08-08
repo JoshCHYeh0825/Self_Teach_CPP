a664b2faecf734c7e9cc771963b9f6ae87ab9e05
Josh CH Yeh <jy0825@bu.edu>
2025-08-08 16:32:21 -0400

    Create Data_Types.cpp

---
+++ Data_Types.cpp
@@ -0,0 +1,21 @@
+/*
+
+Variables are what allows the program to store and manipulate data i.e position of a sprite in a game.
+
+Data as variables are stored in the stack or the heap, within the memory.
+
+Primitive data types are the building blocks of data types in memory,
+they have specific purposes but we can use it not for those purposes.
+
+The only distinction type is their size.
+
+int: Whole number data type, 4 Bytes large (8b per Byte, 32b in total, compiler dependent), ranges from -(2^31) <--> (2^31 - 1).
+unsigned int: int data type but can only exist as an absolute/positive whole number, range from 0 <--> 2^32.
+char: Characters, 1B of data, can be assigned as character or number
+short: 2B of data
+long: 4B of data (Compiler dependent)
+long long: 8B of data
+
+
+
+*/
