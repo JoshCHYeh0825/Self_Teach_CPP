7f589cd45ad810db2cc8f515adc11eeedcf50ddf
Josh CH Yeh <jy0825@bu.edu>
2025-08-10 16:35:06 -0400

    Update Main.cpp

--- Cherno Tutorials/Branches & Conditions/Main.cpp
+++ Cherno Tutorials/Branches & Conditions/Main.cpp
@@ -3,6 +3,19 @@
 if statements and branches would carry overhead because you need to check the condition and branch/jump.
 True = 1, False = 0
 
+Execute conditional code if true, skips if false.
+
+Branching/comparitors are a good way to check if a pointer is null
+e.g)
+
+const char* ptr = "hello";
+if (ptr){
+
+or 
+
+ptr = 
+
+}
 */
 
 #include <iostream>
@@ -10,12 +23,12 @@ True = 1, False = 0
 
 int main()
 {
-    int x = 5;
-    bool ComparisonResult = x == 5;
-    if(ComparisonResult) // if(boolean) is the same as typing if(boolean == true)
-    {
-        Log("Hello World!");
-    }
+    const char* ptr = nullptr;
+    if (ptr)
+        Log(ptr);
+    else
+        Log("ptr is null");
+        
     std::cin.get();
 }
 
