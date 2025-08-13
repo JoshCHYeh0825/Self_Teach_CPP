55590e364eeb9532ebfb99d692d99f7236c6e7c7
Josh CH Yeh <jy0825@bu.edu>
2025-08-13 14:00:20 -0400

    Finished tutorial for control flow

--- Cherno Tutorials/Control Flow/return.cpp
+++ Cherno Tutorials/Control Flow/return.cpp
@@ -5,8 +5,11 @@ int main()
     for(int i = 0; i < 5; i++)
     {
         if((i + 1) % 2 == 0)
-            break; //The moment the condition is satisfied and meets "break" the loop is over.
-            //Loop body only runs once due to "break"
+            return 0; // The moment the program reaches here the function main() stops working immediately.
+            // Only return at the end of the function or when you want the program to run through a certain stage before stopping.
+
+            //Unlike break and continue, return can be put anywhere within a function i.e return 0 for int main().
+            //Unless the function is a void function, you have to return the data type the function is declared as.
         std::cout << "Hello World!" << std::endl;
         std::cout << i << std::endl;
    }
---
+++ Cherno Tutorials/Pointers/Pointers.cpp
@@ -0,0 +1,5 @@
+/*
+
+Raw pointers (Smart Poitners later)
+
+*/
