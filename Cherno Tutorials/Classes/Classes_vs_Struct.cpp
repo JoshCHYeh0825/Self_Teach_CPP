d3d8ba55eeafc5928a1c6a8a701139219da5dc92
Josh CH Yeh <jy0825@bu.edu>
2025-08-15 17:14:54 -0400

    Create Classes_vs_Struct.cpp

---
+++ Cherno Tutorials/Classes/Classes_vs_Struct.cpp
@@ -0,0 +1,18 @@
+/*
+
+Class vs Struct 
+
+There are basically no difference. Just one regarding visibility.
+
+Class members are ;rivate on default unless declared public otherwise.
+Which is why when we declare a class it goes like:
+
+class Class_Name
+{
+public:
+    members
+}
+
+
+
+*/
