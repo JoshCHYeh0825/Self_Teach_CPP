7904253c194ac62e932dd64270d8f8148e114873
Josh CH Yeh <jy0825@bu.edu>
2025-08-15 18:29:12 -0400

    Commence long class tutorial

--- Cherno Tutorials/Classes/Classes_vs_Struct.cpp
+++ Cherno Tutorials/Classes/Classes_vs_Struct.cpp
@@ -13,6 +13,28 @@ public:
     members
 }
 
+Struct's access are defaulted to public, different to classes.
 
+When do we use struct vs class?
 
+Struct: Used mainly just for Plain Old Data (POD).
+e.g)
+A struct called Vec2 which is a representation of 2 floats.
+THere is a method but all it does is just manipulalte the variables.
+
+struct Vec2
+{
+    float x, y;
+
+    void Add(const Vec2& other)
+    {
+        x += other.x;
+        y += other.y;
+    }
+}
+
+You would preferrably use a class for more complex reasons.
+
+You should not use inheritance with structs. If you have hierarchy you should use class.
+Use struct for structures of data, that is literally why it is called struct. 
 */
