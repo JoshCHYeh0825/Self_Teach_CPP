28730b9bea888a60b024e79480e6f1b59a894ae6
Josh CH Yeh <jy0825@bu.edu>
2025-08-19 18:19:56 -0400

    Update ENUMs.cpp

---
+++ Cherno Tutorials/ENUMs.cpp
@@ -0,0 +1,15 @@
+/*
+
+ENUM short for enumerations.
+A set of values, a way to give a name to a value.
+Instead of having a bunch of integers called A,B,C,.. etc.
+We can have an ENUM which has the value of A, B, C which are valued to those integers
+
+To simplify, it's a way to name values
+
+TLDR it's an integer.
+
+*/
+
+#include <iostream>
+
