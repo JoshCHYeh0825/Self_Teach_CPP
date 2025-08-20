00bb7c85977713ee81ffa4f0acc44bf6eec4ec23
Josh CH Yeh <jy0825@bu.edu>
2025-08-20 15:14:38 -0400

    ENUM tutorial

--- Cherno Tutorials/ENUMs.cpp
+++ Cherno Tutorials/ENUMs.cpp
@@ -11,5 +11,34 @@ TLDR it's an integer.
 
 */
 
+enum Example : char
+{
+    A = 5, B = 6, C = 7 // Althought they are declared as A, B, C, they are still values at the end of the day
+
+    //By default the first value would be 0 and it would increment.
+};
+
 #include <iostream>
 
+int a = 0;
+int b = 1;
+int c = 2;
+
+/*
+
+Biggest issue regarding A, B, C is that they aren't grouped
+ENUM would group them together
+
+*/
+
+int main()
+{
+    Example value = B;
+
+    if(value == 1)
+    {
+        // some action here
+    }
+
+    std::cin.get();
+}
