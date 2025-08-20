b07d0d8201808fc8ed9f12b0cf34ffdfd68ac791
Josh CH Yeh <jy0825@bu.edu>
2025-08-20 15:52:19 -0400

    Initiated Init constructor class

--- Cherno Tutorials/Constructors & Destructors/Constructors.cpp
+++ Cherno Tutorials/Constructors & Destructors/Constructors.cpp
@@ -8,11 +8,26 @@ Constructor: Type of method that runs everytime we instantiate an object
 
 class Entity
 {
-    public:
-        float X, Y;  
+public:
+    float X, Y;  
+
+    void Init()
+    {
+        X = 0.0f;
+        Y = 0.0f;
+    }
+    
+    void Print()
+    {
+        std::cout << X << ", " << Y << std::endl;
+    }
 };
 
 int main()
 {
+    Entity e;
+    e.Init();
+    std::cout << e.X << std::endl;
+    e.Print();
     std::cin.get();
 }
