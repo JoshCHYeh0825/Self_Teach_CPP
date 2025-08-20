3a6ac8df9cbde1627fc4d577cf3f1a91af06dce4
Josh CH Yeh <jy0825@bu.edu>
2025-08-20 15:47:13 -0400

    Update Constructors.cpp

--- Cherno Tutorials/Constructors & Destructors/Constructors.cpp
+++ Cherno Tutorials/Constructors & Destructors/Constructors.cpp
@@ -1,1 +1,18 @@
+/*
+
+Constructor: Type of method that runs everytime we instantiate an object
+
+*/
+
 #include <iostream>
+
+class Entity
+{
+    public:
+        float X, Y;  
+};
+
+int main()
+{
+    std::cin.get();
+}
